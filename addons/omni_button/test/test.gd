b0e24bcfd934d6cba9b3857909b3455c0c684efb
Lokix84 <loki.zero@gmail.com>
2025-09-26 13:50:43 -0400

    removed gd script
    will be making a GDscript verison of the plugin eventually

--- addons/omni_button/test/test.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _ready() -> void:
-	$IconButton.DisplayTexture(load("res://addons/omni_button/OmniButton.png"),true)
-	$IconButton.PressedAction = func(): print("new pressed functionality")
-	$IconButton.ReleasedAction = func(): print("new released functionality")
-	$LabelButton.DisplayLabel("Testing")
